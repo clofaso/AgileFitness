c22234c7bea250cbec8dea7dcf7837244b43b459
Christine LoFaso <christine.lofaso@gmail.com>
2021-11-04 11:29:50 -0400

    Implemented internal app calendar

--- app/src/main/java/com/example/finalprojectbcs430W/Calendar.java
+++ app/src/main/java/com/example/finalprojectbcs430W/Calendar.java
@@ -3,12 +3,23 @@ package com.example.finalprojectbcs430W;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
 
 public class Calendar extends AppCompatActivity {
+
+    CalendarView calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        calendar = findViewById(R.id.calendarView);
+    }
+
+    public void Currentdate(View view) {
+        long time = System.currentTimeMillis();
+        calendar.setDate(time,false,true);
     }
 }
